d9d55dae74775d95e3910cc243b6c5528d8dbdec
Ajay Tidke <ajaytidke28297@gmail.com>
2023-01-09 00:25:10 +0530

    spring password encorder

--- src/main/java/com/explanatory/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/explanatory/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,9 +2,20 @@ package com.explanatory.learnspringsecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -17,7 +28,58 @@ public class BasicAuthSecurityConfiguration {
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.httpBasic();
         httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().sameOrigin();
         return httpSecurity.build();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        var user = User.withUsername("explanatory")
+//                .password("{noop}dummy")
+//                .roles("USER")
+//                .build();
+//
+//        var admin = User.withUsername("admin")
+//                .password("{noop}dummy")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION).build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+
+        var user = User.withUsername("explanatory").password("dummy").passwordEncoder(str -> passwordEncoder().encode(str)).roles("USER").build();
+
+        var admin = User.withUsername("admin").password("dummy").passwordEncoder(str -> passwordEncoder().encode(str)).roles("ADMIN", "USER").build();
+
+        var jdbc = new JdbcUserDetailsManager(dataSource);
+        jdbc.createUser(user);
+        jdbc.createUser(admin);
+
+        return jdbc;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
